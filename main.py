c3553b450e31a90a47cbae40e6d5071cace328bb
Tanay <tanaymatta27@gmail.com>
2023-07-11 11:47:53 +0400

    added transaction historYB D D dddd

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append( './src/' )
 from src.BankServices import BankServices as BankServices
 from src.NewAccount import NewAccount as NewAccount
+from src.TransactionHistory import TransactionHistory as Trhist
 def main():
     choice = int(input('Enter 1 to open a new account or 2 to access an existing account or 3 to delete an existing account : '))
     if choice == 1:
@@ -14,15 +15,21 @@ def main():
         if choice == 1:
             amount = int(input('Enter the amount to be deposited: '))
             x.deposit(amount)
+            y = Trhist(userid,amount)
+            y.update_to_Transaction_history_csv()
         elif choice == 2:
             amount = int(input('Enter the amount to be withdrawn: '))
             x.withdraw(amount)
+            y = Trhist(userid,amount)
+            y.update_to_Transaction_history_csv()
         elif choice == 3:
             x.check_balance()
         elif choice == 4:
             amount = int(input('Enter the amount to be transferred: '))
             userid = int(input('Enter the user id of the person to whom the amount is to be transferred: '))
             x.transfer(amount,userid)
+            y = Trhist(userid,amount)
+            y.update_to_Transaction_history_csv()
         else:
             print('Invalid choice')
     elif choice == 3:
